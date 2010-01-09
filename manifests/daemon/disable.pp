2125019efdf0a770eddafaaa58ddf50153cf9f2b
mh <mh@immerda.ch>
2010-01-09 15:45:45 +0100

    disable is already set

--- manifests/daemon/disable.pp
+++ manifests/daemon/disable.pp
@@ -1,8 +1,5 @@
 class gitosis::daemon::disable inherits gitosis::daemon {
   include ::git::daemon::disable
-  File['/etc/sysconfig/git-daemon']{
-    ensure => absent,
-  }
   User::Managed['gitosisd']{
     ensure => 'absent'
   }
