fb23671646bf12c0b80783c36b2a8dabd88fa2e5
mh <mh@immerda.ch>
2010-01-09 15:37:32 +0100

    fix lookup

--- manifests/daemon/disable.pp
+++ manifests/daemon/disable.pp
@@ -1,5 +1,5 @@
 class gitosis::daemon::disable inherits gitosis::daemon {
-  include git::daemon::disable
+  include ::git::daemon::disable
   File['/etc/sysconfig/git-daemon']{
     ensure => absent,
   }
