cc46e62518809be656e598291dbbb57bc9828ae4
mh <mh@immerda.ch>
2010-10-21 03:29:16 +0200

    use new require statement

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,10 +1,9 @@
 class gitosis::base {
-    include git
-    include rsync::client
-    include python::setuptools
-    package{'gitosis':
-        ensure => installed,
-        require => [ Package['git'], Package['rsync'], Package['python-setuptools'] ],
-    }
+  require git
+  require rsync::client
+  require python::setuptools
+  package{'gitosis':
+    ensure => installed,
+  }
 }
 
